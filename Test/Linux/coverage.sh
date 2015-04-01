1b6b9d7a364eb0c57934b8d4c0ff362a7d0502a8
vczh <vczh@163.com>
2015-04-01 11:24:31 +0800

    coverage.sh

---
+++ Test/Linux/coverage.sh
@@ -0,0 +1,16 @@
+mkdir Bin
+mkdir Obj
+cp makefile makefile~
+MakeGen debug makefile
+make clean
+make
+cp makefile~ makefile
+rm makefile~
+Bin/UnitTest
+mkdir Coverage
+cd Coverage
+gcov -o ../Obj/ ../Source/Utility/*.cpp ../../Source/*.cpp ../../Source/Collections/*.h ../../Source/Parsing/*.cpp ../../Source/Reflection/*.cpp ../../Source/Regex/*.cpp ../../Source/Stream/*.cpp
+lcov --directory ../Obj/ --capture --output-file lcov.info
+genhtml -o HTML lcov.info
+echo Generated Files:
+ls -la
