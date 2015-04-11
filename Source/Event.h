833bb7b02b1d15fd469ba5a11ca1cea02137a8c1
vczh <vczh@163.com>
2015-04-10 20:45:53 -0700

    Add document.

--- Source/Event.h
+++ Source/Event.h
@@ -8,8 +8,10 @@ Classes:
 ***********************************************************************/
 #ifndef VCZH_EVENT
 #define VCZH_EVENT
+
 #include "Function.h"
 #include "Collections/List.h"
+
 namespace vl
 {
 	template<typename T>
@@ -23,6 +25,8 @@ namespace vl
 		virtual bool							IsAttached() = 0;
 	};
 
+	/// <summary>Event.</summary>
+	/// <typeparam name="TArgs">Types of callback parameters.</typeparam>
 	template<typename ...TArgs>
 	class Event<void(TArgs...)> : public Object, private NotCopyable
 	{
@@ -47,6 +51,9 @@ namespace vl
  
 		collections::SortedList<Ptr<EventHandlerImpl>>	handlers;
 	public:
+		/// <summary>Add a callback to the event.</summary>
+		/// <returns>The event handler representing the callback.</returns>
+		/// <param name="function">The callback.</param>
 		Ptr<EventHandler> Add(const Func<void(TArgs...)>& function)
 		{
 			Ptr<EventHandlerImpl> handler = new EventHandlerImpl(function);
@@ -54,18 +61,28 @@ namespace vl
 			return handler;
 		}
  
-		template<typename C>
+		/// <summary>Add a callback to the event.</summary>
+		/// <returns>The event handler representing the callback.</returns>
+		/// <param name="function">The callback.</param>
 		Ptr<EventHandler> Add(void(*function)(TArgs...))
 		{
 			return Add(Func<void(TArgs...)>(function));
 		}
  
+		/// <summary>Add a method callback to the event.</summary>
+		/// <typeparam name="C">Type of the class that has the method callback.</typeparam>
+		/// <returns>The event handler representing the callback.</returns>
+		/// <param name="sender">The object that has the method callback.</param>
+		/// <param name="function">The method callback.</param>
 		template<typename C>
 		Ptr<EventHandler> Add(C* sender, void(C::*function)(TArgs...))
 		{
 			return Add(Func<void(TArgs...)>(sender, function));
 		}
  
+		/// <summary>Remove a callback.</summary>
+		/// <returns>Returns true if this operation is succeeded.</returns>
+		/// <param name="handler">The event handler representing the callback.</param>
 		bool Remove(Ptr<EventHandler> handler)
 		{
 			Ptr<EventHandlerImpl> impl = handler.Cast<EventHandlerImpl>();
@@ -77,6 +94,8 @@ namespace vl
 			return true;
 		}
  
+		/// <summary>Invoke all callbacks in the event.</summary>
+		/// <param name="args">Arguments to invoke all callbacks.</param>
 		void operator()(TArgs ...args)const
 		{
 			for(vint i = 0; i < handlers.Count(); i++)
