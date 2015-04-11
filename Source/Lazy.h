c801f2454323104d2621ba96505e93957461d5a5
vczh <vczh@163.com>
2015-04-11 00:28:30 -0700

    Add document.

--- Source/Lazy.h
+++ Source/Lazy.h
@@ -16,6 +16,8 @@ Classes:
 
 namespace vl
 {
+	/// <summary>A type representing a lazy evaluation.</summary>
+	/// <typeparam name="T">The type of the evaluation result.</typeparam>
 	template<typename T>
 	class Lazy : public Object
 	{
@@ -30,10 +32,13 @@ namespace vl
 
 		Ptr<Internal>			internalValue;
 	public:
+		/// <summary>Create an empty evaluation.</summary>
 		Lazy()
 		{
 		}
 
+		/// <summary>Create an evaluation using a function.</summary>
+		/// <param name="evaluator">The function.</param>
 		Lazy(const Func<T()>& evaluator)
 		{
 			internalValue=new Internal;
@@ -41,6 +46,8 @@ namespace vl
 			internalValue->evaluator=evaluator;
 		}
 
+		/// <summary>Create an evaluation using the result directly.</summary>
+		/// <param name="value">The result that you have already known.</param>0
 		Lazy(const T& value)
 		{
 			internalValue=new Internal;
@@ -48,6 +55,8 @@ namespace vl
 			internalValue->value=value;
 		}
 
+		/// <summary>Copy an evaluation.</summary>
+		/// <param name="lazy">The evaluation to copy.</param>
 		Lazy(const Lazy<T>& lazy)
 			:internalValue(lazy.internalValue)
 		{
@@ -75,6 +84,8 @@ namespace vl
 			return *this;
 		}
 
+		/// <summary>Get the evaluation result. If it has not been calculated yet, it will run the evaluation and cache the result. You will not need to calculate for the second time.</summary>
+		/// <returns>The evaluation result.</returns>
 		const T& Value()const
 		{
 			if(!internalValue->evaluated)
@@ -86,11 +97,15 @@ namespace vl
 			return internalValue->value;
 		}
 
+		/// <summary>Test if it has already been evaluated or not.</summary>
+		/// <returns>Returns true if it has already been evaluated.</returns>
 		const bool IsEvaluated()const
 		{
 			return internalValue->evaluated;
 		}
 
+		/// <summary>Test if it is an empty evaluation or not.</summary>
+		/// <returns>Returns true if it is not empty.</returns>
 		const bool IsAvailable()const
 		{
 			return internalValue;
