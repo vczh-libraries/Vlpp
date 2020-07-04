a0fe80170f2bc0e4a5baf5446529bcec28541caf
Zihan Chen <vczh@163.com>
2020-07-03 17:31:58 -0700

    Update Console.h

--- Source/Console.h
+++ Source/Console.h
@@ -14,29 +14,29 @@ namespace vl
 {
 	namespace console
 	{
-		/// <summary>A Static class for command line window operations.</summary>
+		/// <summary>Basic I/O for command-line applications.</summary>
 		class Console abstract
 		{
 		public:
-			/// <summary>Write to the command line window.</summary>
+			/// <summary>Write a string to the command-line window.</summary>
 			/// <param name="string">Content to write.</param>
-			/// <param name="length">Size of the content in wchar_t. The zero terminator is not included.</param>
+			/// <param name="length">Size of the content in wchar_t, not including the zero terminator.</param>
 			static void Write(const wchar_t* string, vint length);
 
-			/// <summary>Write to the command line window.</summary>
-			/// <param name="string">Content to write.</param>
+			/// <summary>Write a string to the command-line window.</summary>
+			/// <param name="string">Content to write, must be zero terminated.</param>
 			static void Write(const wchar_t* string);
 
-			/// <summary>Write to the command line window.</summary>
+			/// <summary>Write a string to the command-line window.</summary>
 			/// <param name="string">Content to write.</param>
 			static void Write(const WString& string);
 
-			/// <summary>Write to the command line window with a CRLF.</summary>
+			/// <summary>Write to the command-line window, following CR/LF characters.</summary>
 			/// <param name="string">Content to write.</param>
 			static void WriteLine(const WString& string);
 
-			/// <summary>Read from the command line window.</summary>
-			/// <returns>The whole line read from the command line window.</returns>
+			/// <summary>Read a string from the command-line window.</summary>
+			/// <returns>The whole line read from the command-line window.</returns>
 			static WString Read();
 
 			static void SetColor(bool red, bool green, bool blue, bool light);
