3c9468d64beb30251840fb3a4baee495785db502
vczh <vczh@outlook.com>
2021-10-21 18:25:36 -0700

    Update TestUtfReader.cpp

--- Test/Source/Strings/TestUtfReader.cpp
+++ Test/Source/Strings/TestUtfReader.cpp
@@ -8,23 +8,45 @@ using namespace vl::encoding;
 namespace test_utf8_reader
 {
 	template<typename T, vint SourceLength, vint DestLength>
-	void TestTo32(const T(&st)[SourceLength], const char32_t(&s32)[DestLength])
-	{
-	}
-
-	template<typename T, vint SourceLength, vint DestLength>
 	void TestFrom32(const T(&st)[DestLength], const char32_t(&s32)[SourceLength])
 	{
 	}
 
-	template<typename T>
-	void TestTo32Boundary(const T(&st)[UtfConversion<T>::BufferLength], char32_t c32)
+	template<typename T, vint SourceLength, vint DestLength>
+	void TestTo32(const T(&st)[SourceLength], const char32_t(&s32)[DestLength])
 	{
 	}
 
-	template<typename T>
-	void TestFrom32Boundary(const T(&st)[UtfConversion<T>::BufferLength], char32_t c32)
+	template<typename T, vint DestLength>
+	void TestFrom32Boundary(const T(&st)[DestLength], char32_t c32, bool success)
 	{
+		T dest[UtfConversion<T>::BufferLength] = { 0 };
+		vint result = UtfConversion<T>::From32(c32, dest);
+		if (success)
+		{
+			TEST_ASSERT(result == DestLength - 1);
+			TEST_ASSERT(memcpy(dest, st, sizeof(T) * (size_t)result) == 0);
+		}
+		else
+		{
+			TEST_ASSERT(result == -1);
+		}
+	}
+
+	template<typename T, vint SourceLength>
+	void TestTo32Boundary(const T(&st)[SourceLength], char32_t c32, bool success)
+	{
+		char32_t dest = 0;
+		vint result = UtfConversion<T>::To32(st, SourceLength - 1, dest);
+		if (success)
+		{
+			TEST_ASSERT(result == SourceLength - 1);
+			TEST_ASSERT(dest == c32);
+		}
+		else
+		{
+			TEST_ASSERT(result == -1);
+		}
 	}
 }
 using namespace test_utf8_reader;
@@ -35,21 +57,6 @@ TEST_FILE
 	const char8_t textU8[] = u8"𩰪㦲𦰗𠀼 𣂕𣴑𣱳𦁚 Vczh is genius!@我是天才";
 	const char16_t textU16[] = u"𩰪㦲𦰗𠀼 𣂕𣴑𣱳𦁚 Vczh is genius!@我是天才";
 	const char32_t textU32[] = U"𩰪㦲𦰗𠀼 𣂕𣴑𣱳𦁚 Vczh is genius!@我是天才";
-
-	TEST_CATEGORY(L"wchar_t -> char32_t")
-	{
-		TestTo32(textL, textU32);
-	});
-
-	TEST_CATEGORY(L"char8_t -> char32_t")
-	{
-		TestTo32(textU8, textU32);
-	});
-
-	TEST_CATEGORY(L"char16_t -> char32_t")
-	{
-		TestTo32(textU16, textU32);
-	});
 
 	TEST_CATEGORY(L"char32_t -> wchar_t")
 	{
@@ -66,19 +73,34 @@ TEST_FILE
 		TestFrom32(textU16, textU32);
 	});
 
-	TEST_CATEGORY(L"char8_t -> char32_t boundary")
+	TEST_CATEGORY(L"wchar_t -> char32_t")
 	{
+		TestTo32(textL, textU32);
+	});
+
+	TEST_CATEGORY(L"char8_t -> char32_t")
+	{
+		TestTo32(textU8, textU32);
+	});
+
+	TEST_CATEGORY(L"char16_t -> char32_t")
+	{
+		TestTo32(textU16, textU32);
 	});
 
 	TEST_CATEGORY(L"char32_t -> char8_t boundary")
 	{
 	});
 
-	TEST_CATEGORY(L"char16_t -> char32_t boundary")
+	TEST_CATEGORY(L"char8_t -> char32_t boundary")
 	{
 	});
 
 	TEST_CATEGORY(L"char32_t -> char16_t boundary")
+	{
+	});
+
+	TEST_CATEGORY(L"char16_t -> char32_t boundary")
 	{
 	});
 }
