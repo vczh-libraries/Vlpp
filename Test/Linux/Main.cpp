b57b64460dd0a2414706bec9c2856b17e8abbc67
vczh <vczh@outlook.com>
2021-10-22 16:12:17 -0700

    ...

--- Test/Linux/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../Source/UnitTest/UnitTest.h"
-
-using namespace vl;
-
-int main(int argc, char** argv)
-{
-	return unittest::UnitTest::RunAndDisposeTests(argc, argv);
-}
--- Test/UnitTest/UnitTest/Main.cpp
+++ Test/UnitTest/UnitTest/Main.cpp
@@ -3,10 +3,14 @@
 
 using namespace vl;
 
+#ifdef VCZH_MSVC
 int wmain(int argc , wchar_t* argv[])
+#else
+int main(int argc, char** argv)
+#endif
 {
 	int result = unittest::UnitTest::RunAndDisposeTests(argc, argv);
-	FinalizeGlobalStorage();
+	vl::FinalizeGlobalStorage();
 #ifdef VCZH_CHECK_MEMORY_LEAKS
 	_CrtDumpMemoryLeaks();
 #endif
