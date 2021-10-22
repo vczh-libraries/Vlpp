bb87e41654f11d2398ffc8f2f9d8a179db7a5d21
vczh <vczh@outlook.com>
2021-10-21 17:18:17 -0700

    Update Conversion.cpp

--- Source/Strings/Conversion.cpp
+++ Source/Strings/Conversion.cpp
@@ -36,17 +36,73 @@ UtfConversion<wchar_t>
 UtfConversion<char8_t>
 ***********************************************************************/
 
+		/*
+		How UCS-4 translates to UTF-8
+			U-00000000 - U-0000007F:  0xxxxxxx
+			U-00000080 - U-000007FF:  110xxxxx 10xxxxxx
+			U-00000800 - U-0000FFFF:  1110xxxx 10xxxxxx 10xxxxxx
+			U-00010000 - U-001FFFFF:  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+			U-00200000 - U-03FFFFFF:  111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			U-04000000 - U-7FFFFFFF:  1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+		*/
+
 		vint UtfConversion<char8_t>::From32(char32_t source, char8_t(&dest)[BufferLength])
 		{
 		}
 
 		void UtfConversion<char8_t>::To32(const char8_t(&source)[BufferLength], char32_t& dest)
 		{
+			const vuint8_t(&cs)[BufferLength] = reinterpret_cast<const vuint8_t(&)[BufferLength]>(source);
+			vuint64_t& d = reinterpret_cast<vuint64_t&>(dest);
+
+			if (cs[0] < 0b10000000U)
+			{
+				d = cs[0];
+			}
+			else if (cs[0] < 0b11100000U)
+			{
+				d = ((static_cast<vuint64_t>(cs[0]) & static_cast<vuint64_t>(0b00011111U)) << 6) |
+					((static_cast<vuint64_t>(cs[1]) & static_cast<vuint64_t>(0b00111111U)));
+			}
+			else if (cs[0] < 0b11110000U)
+			{
+				d = ((static_cast<vuint64_t>(cs[0]) & static_cast<vuint64_t>(0b00001111U)) << 12) |
+					((static_cast<vuint64_t>(cs[1]) & static_cast<vuint64_t>(0b00111111U)) << 6) |
+					((static_cast<vuint64_t>(cs[2]) & static_cast<vuint64_t>(0b00111111U)));
+			}
+			else if (cs[0] < 0b11111000U)
+			{
+				d = ((static_cast<vuint64_t>(cs[0]) & static_cast<vuint64_t>(0b00000111U)) << 18) |
+					((static_cast<vuint64_t>(cs[1]) & static_cast<vuint64_t>(0b00111111U)) << 12) |
+					((static_cast<vuint64_t>(cs[2]) & static_cast<vuint64_t>(0b00111111U)) << 6) |
+					((static_cast<vuint64_t>(cs[3]) & static_cast<vuint64_t>(0b00111111U)));
+			}
+			else if (cs[0] < 0b11111100U)
+			{
+				d = ((static_cast<vuint64_t>(cs[0]) & static_cast<vuint64_t>(0b00000011U)) << 24) |
+					((static_cast<vuint64_t>(cs[1]) & static_cast<vuint64_t>(0b00111111U)) << 18) |
+					((static_cast<vuint64_t>(cs[2]) & static_cast<vuint64_t>(0b00111111U)) << 12) |
+					((static_cast<vuint64_t>(cs[3]) & static_cast<vuint64_t>(0b00111111U)) << 6) |
+					((static_cast<vuint64_t>(cs[4]) & static_cast<vuint64_t>(0b00111111U)));
+			}
+			else
+			{
+				d = ((static_cast<vuint64_t>(cs[0]) & static_cast<vuint64_t>(0b00000001U)) << 30) |
+					((static_cast<vuint64_t>(cs[1]) & static_cast<vuint64_t>(0b00111111U)) << 24) |
+					((static_cast<vuint64_t>(cs[2]) & static_cast<vuint64_t>(0b00111111U)) << 18) |
+					((static_cast<vuint64_t>(cs[3]) & static_cast<vuint64_t>(0b00111111U)) << 12) |
+					((static_cast<vuint64_t>(cs[4]) & static_cast<vuint64_t>(0b00111111U)) << 6) |
+					((static_cast<vuint64_t>(cs[5]) & static_cast<vuint64_t>(0b00111111U)));
+			}
 		}
 
 /***********************************************************************
 UtfConversion<char16_t>
 ***********************************************************************/
+
+		/*
+		How UCS-4 translates to UTF-16
+		*/
 
 		vint UtfConversion<char16_t>::From32(char32_t source, char16_t(&dest)[BufferLength])
 		{
