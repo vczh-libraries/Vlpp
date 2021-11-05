a1107171f24d4a0b16de51ea9c3e85985bd25580
vczh <vczh@outlook.com>
2021-11-05 16:54:31 -0700

    Fix ubuntu build breaks

--- Source/UnitTest/UnitTest.Linux.cpp
+++ Source/UnitTest/UnitTest.Linux.cpp
@@ -4,7 +4,6 @@ Licensed under https://github.com/vczh-libraries/License
 ***********************************************************************/
 
 #include "UnitTest.h"
-#include <Windows.h>
 
 #ifndef VCZH_GCC
 static_assert(false, "Do not build this file for Windows applications.");
