382b9c9e4709dd4ffd8fa6288ab5dafcff0df28d
vczh <vczh@outlook.com>
2023-01-19 00:33:22 -0800

    ...

--- Test/Source/TestSort.cpp
+++ Test/Source/TestSort.cpp
@@ -68,15 +68,36 @@ TEST_FILE
 		}
 	});
 
+	TEST_CASE(L"Test SortLambda with one 10k zeros, ones, and twos")
+	{
+		constexpr vint Repeat = 10000;
+		Array<vint> numbers(Repeat * 3);
+		for (vint i = 0; i < Repeat; i++)
+		{
+			numbers[i * 3] = 1;
+			numbers[i * 3 + 1] = 0;
+			numbers[i * 3 + 2] = 2;
+		}
+		SortLambda(&numbers[0], numbers.Count(), Compare);
+
+		for (vint i = 0; i < numbers.Count(); i++)
+		{
+			if (numbers[i] != i / Repeat)
+			{
+				TEST_ASSERT(false);
+			}
+		}
+	});
+
 	TEST_CASE(L"Test SortLambda with one 100k zeros")
 	{
-		Array<vint> zeros(100000);
-		memset(&zeros[0], 0, sizeof(vint) * zeros.Count());
-		SortLambda(&zeros[0], zeros.Count(), Compare);
+		Array<vint> numbers(100000);
+		memset(&numbers[0], 0, sizeof(vint) * numbers.Count());
+		SortLambda(&numbers[0], numbers.Count(), Compare);
 
-		for (vint i = 0; i < zeros.Count(); i++)
+		for (vint i = 0; i < numbers.Count(); i++)
 		{
-			if (zeros[i] != 0)
+			if (numbers[i] != 0)
 			{
 				TEST_ASSERT(false);
 			}
