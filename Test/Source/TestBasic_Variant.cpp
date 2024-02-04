dd54e69bc677f279ae956d916f1aac816c9ec1be
vczh <vczh@outlook.com>
2024-02-03 23:37:54 -0800

    ...

--- Test/Source/TestBasic_Variant.cpp
+++ Test/Source/TestBasic_Variant.cpp
@@ -5,6 +5,86 @@
 
 using namespace vl;
 
+namespace TestVariant_TestObjects
+{
+	void AssertEmptyString(Variant<WString>& v)
+	{
+		TEST_ASSERT(v.Index() == 0);
+		TEST_ASSERT(v.TryGet<WString>() != nullptr);
+		TEST_ASSERT(v.Get<WString>() == WString::Empty);
+	}
+
+	void AssertEmptyString(const Variant<WString>& v)
+	{
+		TEST_ASSERT(v.Index() == 0);
+		TEST_ASSERT(v.TryGet<WString>() != nullptr);
+		TEST_ASSERT(v.Get<WString>() == WString::Empty);
+	}
+
+	void AssertString(Variant<WString>& v)
+	{
+		TEST_ASSERT(v.Index() == 0);
+		TEST_ASSERT(v.TryGet<WString>() != nullptr);
+		TEST_ASSERT(v.Get<WString>() == L"ABC");
+	}
+
+	void AssertString(const Variant<WString>& v)
+	{
+		TEST_ASSERT(v.Index() == 0);
+		TEST_ASSERT(v.TryGet<WString>() != nullptr);
+		TEST_ASSERT(v.Get<WString>() == L"ABC");
+	}
+
+	void AssertEmptyString(Variant<WString, vint>& v)
+	{
+		TEST_ASSERT(v.Index() == 0);
+		TEST_ASSERT(v.TryGet<WString>() != nullptr);
+		TEST_ASSERT(v.TryGet<vint>() == nullptr);
+		TEST_ASSERT(v.Get<WString>() == WString::Empty);
+	}
+
+	void AssertEmptyString(const Variant<WString, vint>& v)
+	{
+		TEST_ASSERT(v.Index() == 0);
+		TEST_ASSERT(v.TryGet<WString>() != nullptr);
+		TEST_ASSERT(v.TryGet<vint>() == nullptr);
+		TEST_ASSERT(v.Get<WString>() == WString::Empty);
+	}
+
+	void AssertString(Variant<WString, vint>& v)
+	{
+		TEST_ASSERT(v.Index() == 0);
+		TEST_ASSERT(v.TryGet<WString>() != nullptr);
+		TEST_ASSERT(v.TryGet<vint>() == nullptr);
+		TEST_ASSERT(v.Get<WString>() == L"ABC");
+	}
+
+	void AssertString(const Variant<WString, vint>& v)
+	{
+		TEST_ASSERT(v.Index() == 0);
+		TEST_ASSERT(v.TryGet<WString>() != nullptr);
+		TEST_ASSERT(v.TryGet<vint>() == nullptr);
+		TEST_ASSERT(v.Get<WString>() == L"ABC");
+	}
+
+	void AssertInt(Variant<WString, vint>& v)
+	{
+		TEST_ASSERT(v.Index() == 1);
+		TEST_ASSERT(v.TryGet<WString>() == nullptr);
+		TEST_ASSERT(v.TryGet<vint>() != nullptr);
+		TEST_ASSERT(v.Get<vint>() == 100);
+	}
+
+	void AssertInt(const Variant<WString, vint>& v)
+	{
+		TEST_ASSERT(v.Index() == 1);
+		TEST_ASSERT(v.TryGet<WString>() == nullptr);
+		TEST_ASSERT(v.TryGet<vint>() != nullptr);
+		TEST_ASSERT(v.Get<vint>() == 100);
+	}
+}
+using namespace TestVariant_TestObjects;
+
 TEST_FILE
 {
 	TEST_CATEGORY(L"Single Element")
@@ -13,15 +93,11 @@ TEST_FILE
 		{
 			{
 				Variant<WString> v;
-				TEST_ASSERT(v.Index() == 0);
-				TEST_ASSERT(v.TryGet<WString>() != nullptr);
-				TEST_ASSERT(v.Get<WString>() == WString::Empty);
+				AssertEmptyString(v);
 			}
 			{
 				const Variant<WString> v;
-				TEST_ASSERT(v.Index() == 0);
-				TEST_ASSERT(v.TryGet<WString>() != nullptr);
-				TEST_ASSERT(v.Get<WString>() == WString::Empty);
+				AssertEmptyString(v);
 			}
 		});
 
@@ -30,16 +106,13 @@ TEST_FILE
 			WString a = WString::Unmanaged(L"ABC");
 
 			Variant<WString> v1 = a;
-			TEST_ASSERT(v1.Index() == 0);
-			TEST_ASSERT(v1.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v1.Get<WString>() == L"ABC");
+			TEST_ASSERT(a == L"ABC");
+			AssertString(v1);
 
 			Variant<WString> v2 = v1;
-			TEST_ASSERT(v2.Index() == 0);
-			TEST_ASSERT(v2.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v2.Get<WString>() == L"ABC");
-
 			TEST_ASSERT(a == L"ABC");
+			AssertString(v1);
+			AssertString(v2);
 		});
 
 		TEST_CASE(L"Test Variant<T> move constructor")
@@ -47,19 +120,13 @@ TEST_FILE
 			WString a = WString::Unmanaged(L"ABC");
 
 			Variant<WString> v1 = std::move(a);
-			TEST_ASSERT(v1.Index() == 0);
-			TEST_ASSERT(v1.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v1.Get<WString>() == L"ABC");
+			TEST_ASSERT(a == WString::Empty);
+			AssertString(v1);
 
 			Variant<WString> v2 = std::move(v1);
-			TEST_ASSERT(v2.Index() == 0);
-			TEST_ASSERT(v2.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v2.Get<WString>() == L"ABC");
-
 			TEST_ASSERT(a == WString::Empty);
-			TEST_ASSERT(v1.Index() == 0);
-			TEST_ASSERT(v1.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v1.Get<WString>() == WString::Empty);
+			AssertEmptyString(v1);
+			AssertString(v2);
 		});
 
 		TEST_CASE(L"Test Variant<T> copy assignment")
@@ -68,16 +135,13 @@ TEST_FILE
 			Variant<WString> v1, v2;
 
 			v1 = a;
-			TEST_ASSERT(v1.Index() == 0);
-			TEST_ASSERT(v1.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v1.Get<WString>() == L"ABC");
+			TEST_ASSERT(a == L"ABC");
+			AssertString(v1);
 
 			v2 = v1;
-			TEST_ASSERT(v2.Index() == 0);
-			TEST_ASSERT(v2.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v2.Get<WString>() == L"ABC");
-
 			TEST_ASSERT(a == L"ABC");
+			AssertString(v1);
+			AssertString(v2);
 		});
 
 		TEST_CASE(L"Test Variant<T> move assignment")
@@ -86,19 +150,13 @@ TEST_FILE
 			Variant<WString> v1, v2;
 
 			v1 = std::move(a);
-			TEST_ASSERT(v1.Index() == 0);
-			TEST_ASSERT(v1.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v1.Get<WString>() == L"ABC");
+			TEST_ASSERT(a == WString::Empty);
+			AssertString(v1);
 
 			v2 = std::move(v1);
-			TEST_ASSERT(v2.Index() == 0);
-			TEST_ASSERT(v2.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v2.Get<WString>() == L"ABC");
-
 			TEST_ASSERT(a == WString::Empty);
-			TEST_ASSERT(v1.Index() == 0);
-			TEST_ASSERT(v1.TryGet<WString>() != nullptr);
-			TEST_ASSERT(v1.Get<WString>() == WString::Empty);
+			AssertEmptyString(v1);
+			AssertString(v2);
 		});
 
 		TEST_CASE(L"Test Variant<T> comparison (SO)")
@@ -115,6 +173,141 @@ TEST_FILE
 	});
 
 	TEST_CATEGORY(L"Multiple Elements")
+	{
+		TEST_CASE(L"Test Variant<T>")
+		{
+			{
+				Variant<WString, vint> v;
+				AssertEmptyString(v);
+			}
+			{
+				const Variant<WString, vint> v;
+				AssertEmptyString(v);
+			}
+		});
+
+		TEST_CASE(L"Test Variant<T> copy constructor")
+		{
+			{
+				WString a = WString::Unmanaged(L"ABC");
+
+				Variant<WString, vint> v1 = a;
+				TEST_ASSERT(a == L"ABC");
+				AssertString(v1);
+
+				Variant<WString, vint> v2 = v1;
+				TEST_ASSERT(a == L"ABC");
+				AssertString(v1);
+				AssertString(v2);
+			}
+			{
+				vint a = 100;
+
+				Variant<WString, vint> v1 = a;
+				AssertInt(v1);
+
+				Variant<WString, vint> v2 = v1;
+				AssertInt(v1);
+				AssertInt(v2);
+			}
+		});
+
+		TEST_CASE(L"Test Variant<T> move constructor")
+		{
+			{
+				WString a = WString::Unmanaged(L"ABC");
+
+				Variant<WString, vint> v1 = std::move(a);
+				TEST_ASSERT(a == WString::Empty);
+				AssertString(v1);
+
+				Variant<WString, vint> v2 = std::move(v1);
+				TEST_ASSERT(a == WString::Empty);
+				AssertEmptyString(v1);
+				AssertString(v2);
+			}
+			{
+				vint a = 100;
+
+				Variant<WString, vint> v1 = std::move(a);
+				AssertInt(v1);
+
+				Variant<WString, vint> v2 = std::move(v1);
+				AssertInt(v1);
+				AssertInt(v2);
+			}
+		});
+
+		TEST_CASE(L"Test Variant<T> copy assignment")
+		{
+			{
+				WString a = WString::Unmanaged(L"ABC");
+				Variant<WString, vint> v1, v2;
+
+				v1 = a;
+				TEST_ASSERT(a == L"ABC");
+				AssertString(v1);
+
+				v2 = v1;
+				TEST_ASSERT(a == L"ABC");
+				AssertString(v1);
+				AssertString(v2);
+			}
+			{
+				vint a = 100;
+				Variant<WString, vint> v1, v2;
+
+				v1 = a;
+				AssertInt(v1);
+
+				v2 = v1;
+				AssertInt(v1);
+				AssertInt(v2);
+			}
+		});
+
+		TEST_CASE(L"Test Variant<T> move assignment")
+		{
+			{
+				WString a = WString::Unmanaged(L"ABC");
+				Variant<WString, vint> v1, v2;
+
+				v1 = std::move(a);
+				TEST_ASSERT(a == WString::Empty);
+				AssertString(v1);
+
+				v2 = std::move(v1);
+				TEST_ASSERT(a == WString::Empty);
+				AssertEmptyString(v1);
+				AssertString(v2);
+			}
+			{
+				vint a = 100;
+				Variant<WString, vint> v1, v2;
+
+				v1 = std::move(a);
+				AssertInt(v1);
+
+				v2 = std::move(v1);
+				AssertInt(v1);
+				AssertInt(v2);
+			}
+		});
+
+		TEST_CASE(L"Test Variant<T...> comparison (SO)")
+		{
+		});
+
+		TEST_CASE(L"Test Variant<T...> comparison (WO)")
+		{
+		});
+
+		TEST_CASE(L"Test Variant<T...> comparison (PO)")
+		{
+		});
+	});
+
+	TEST_CATEGORY(L"Auto type conversion")
 	{
 		TEST_CASE(L"Test Variant<T...>")
 		{
