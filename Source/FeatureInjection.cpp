1517e1316014f8c82958bddc12efa9f711aad419
vczh <vczh@outlook.com>
2025-09-20 02:14:39 -0700

    ...

---
+++ Source/FeatureInjection.cpp
@@ -0,0 +1,13 @@
+/***********************************************************************
+Author: Zihan Chen (vczh)
+Licensed under https://github.com/vczh-libraries/License
+***********************************************************************/
+
+#include "FeatureInjection.h"
+
+namespace vl
+{
+	namespace feature_injection
+	{
+	}
+}
---
+++ Source/FeatureInjection.h
@@ -0,0 +1,18 @@
+/***********************************************************************
+Author: Zihan Chen (vczh)
+Licensed under https://github.com/vczh-libraries/License
+***********************************************************************/
+
+#ifndef VCZH_FEATUREINJECTION
+#define VCZH_FEATUREINJECTION
+
+#include "Basic.h"
+
+namespace vl
+{
+	namespace feature_injection
+	{
+	}
+}
+
+#endif
